63f2b4c0c269eb31eca0a00b83f8f27062b9b4dd
AlexandZuev <zionix007@gmail.com>
2025-07-17 22:08:25 +0300

    add section 1-10 apps, add some changes to sprint2WeeklyChallenge

---
+++ Dr. Angela Yu Course/Lessons/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Aliaksandr Zuyeu on 1.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
