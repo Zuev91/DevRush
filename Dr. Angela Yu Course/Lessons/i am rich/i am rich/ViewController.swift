21b9a24ba8971ea4c03a5ecd2029a3f754fd21ea
AlexandZuev <zionix007@gmail.com>
2025-07-17 22:08:25 +0300

    add section 1-10 apps, add some changes to sprint2WeeklyChallenge

---
+++ Dr. Angela Yu Course/Lessons/i am rich/i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Aliaksandr Zuyeu on 30.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
